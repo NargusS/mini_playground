301d49ff7caaf6d1e172f733168c5dcb359dd052
Alexandre Chane-Law <narguss@MacBook-Air-de-Alexandre.local>
2023-02-28 15:07:05 +0100

    add other player

--- test_project/src/App.tsx
+++ test_project/src/App.tsx
@@ -3,15 +3,11 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function Playground(){
-  const [ctx, setCtx] = useState(null);
-  const [otherctx, setotherCtx] = useState(null);
-  
+  const [otherplayer, setOtherPlayer] = useState(0);
+
   function updateDisplay(event, canvas) {
-    if (ctx !== null)
-    {
-      ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
-    }
-    setCtx(canvas?.getContext('2d'))
+    const ctx = canvas?.getContext('2d');
+    ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
     if (event.nativeEvent.offsetY <= 0)
       ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
@@ -20,18 +16,38 @@ function Playground(){
     else
       ctx.fillRect(canvas.width-(canvas.width * 0.02), canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
   }
+  
+  async function updateOtherplayer(canvas){
 
-  function updateOtherplayer(canvas){
-    if (otherctx !== null)
-    {
-      otherctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
-    }
-    setotherCtx(canvas?.getContext('2d'))
-    otherctx.fillStyle = 'rgba(255, 0, 0, 1)'
-    otherctx.fillRect(0, position, canvas.width * 0.02, canvas.height * 0.1)
   }
 
-  // useEffect(() => {}, [position]);
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas?.getContext('2d');
+    ctx.fillStyle = 'rgba(0, 0, 255, 1)'
+    ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
+    // ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    // ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+  }, [])
+
+  useEffect(() => {
+    const updateOtherplayer = setInterval(() => {
+      const canvas = document.getElementById("canvas");
+      const ctx = canvas?.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
+      setOtherPlayer(value => {
+        if (value < canvas.height-(canvas.height * 0.1))
+          return value + 1
+        else
+          return 0
+      })
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+      ctx.fillRect(0, otherplayer, canvas.width * 0.02, canvas.height * 0.1)
+    }, 10);
+    return () => {
+      clearInterval(updateOtherplayer)
+    }
+  }, [otherplayer])
  return (
   <>
     <canvas id="canvas" width={window.innerWidth/2} height={window.innerHeight/2} className="border-slate-700 border-8" onMouseMove={(evt)=> updateDisplay(evt, document.getElementById("canvas"))} onMouseEnter={(evt)=> updateDisplay(evt, document.getElementById("canvas"))} onMouseLeave={(evt)=> updateDisplay(evt, document.getElementById("canvas"))}></canvas>
