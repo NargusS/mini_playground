7c6035bf27dd1884e51aa09a82f68dd35046a625
Alexandre Chane-Law <narguss@MacBook-Air-de-Alexandre.local>
2023-03-01 17:01:28 +0100

    v2

--- back_ws/src/ws_game/ws_game.gateway.ts
+++ back_ws/src/ws_game/ws_game.gateway.ts
@@ -3,13 +3,14 @@ import { WsGameService } from './ws_game.service';
 import { CreateWsGameDto } from './dto/create-ws_game.dto';
 import { UpdateWsGameDto } from './dto/update-ws_game.dto';
 import { Server } from 'socket.io';
-import { Socket } from 'dgram';
+import { Socket } from 'socket.io';
 import { OnModuleInit } from '@nestjs/common';
 
 @WebSocketGateway(4242, {transports:['websocket'], cors:true})
 export class WsGameGateway implements OnModuleInit{
   @WebSocketServer()
   server: Server;
+  number_of_player: number = 0;
   constructor(private readonly wsGameService: WsGameService) {}
   
   //creer un tableau d'objet room {room_name, number_of_player or clients[], scores[], is_playing}
@@ -18,13 +19,24 @@ export class WsGameGateway implements OnModuleInit{
     this.server.on("connection",(socket) =>{
       console.log(socket.id);
       console.log("Connected");
+      this.number_of_player++;
+      this.server.emit('playerid', {id:socket.id,player:this.number_of_player});
     })
+  }
+  
+  @SubscribeMessage('disconnect')
+  disconnect(@ConnectedSocket() client: Socket):void{
+    this.number_of_player--;
+  }
+
+  @SubscribeMessage('playerid')
+  playerid(@ConnectedSocket() client: Socket, @MessageBody()data):void{
+    this.server.except([client.id]).emit('playerid', {id:client.id,player:this.number_of_player});
   }
 
   @SubscribeMessage('test')
-  test(client:Socket, @MessageBody() data:string) {
-    console.log(data + client);
-    return data + {client};
+  test(@ConnectedSocket() client: Socket, @MessageBody() data):void{
+    this.server.except([client.id]).emit('test', data);
   }
 
 }
--- test_project/src/App.tsx
+++ test_project/src/App.tsx
@@ -1,54 +1,94 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import io from 'socket.io-client'
+
+const socket = io('http://10.18.199.128:4242', {transports: ['websocket']});
 
 function Playground(){
   const [otherplayer, setOtherPlayer] = useState(0);
+  const [player, setPlayer] = useState(0);
 
   function updateDisplay(event, canvas) {
     const ctx = canvas?.getContext('2d');
-    ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
+    if (player == 1)
+      ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
+    else if (player == 2)
+      ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-    if (event.nativeEvent.offsetY <= 0)
+    if (event.nativeEvent.offsetY <= 0){
+      if (player == 1)
+        ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+      else if (player == 2)
       ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
-    else if (event.nativeEvent.offsetY < canvas.height-(canvas.height * 0.1))
-      ctx.fillRect(canvas.width-(canvas.width * 0.02), event.nativeEvent.offsetY, canvas.width * 0.02, canvas.height * 0.1)
-    else
-      ctx.fillRect(canvas.width-(canvas.width * 0.02), canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
+      socket.emit('test', {player:player, position: 0});
+    }
+    else if (event.nativeEvent.offsetY < canvas.height-(canvas.height * 0.1)){
+      if (player == 1)
+        ctx.fillRect(0, event.nativeEvent.offsetY, canvas.width * 0.02, canvas.height * 0.1)
+      else if (player == 2)
+        ctx.fillRect(canvas.width-(canvas.width * 0.02), event.nativeEvent.offsetY, canvas.width * 0.02, canvas.height * 0.1)
+      socket.emit('test', {player:player, position: event.nativeEvent.offsetY});
+    }
+    else{
+      if (player == 1)
+        ctx.fillRect(0, canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
+      else if (player == 2)
+        ctx.fillRect(canvas.width-(canvas.width * 0.02), canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
+      socket.emit('test', {player:player, position: canvas.height-(canvas.height * 0.1)});
+    }
   }
-  
-  async function updateOtherplayer(canvas){
 
-  }
 
   useEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas?.getContext('2d');
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-    ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
-    // ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-    // ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+    if (player == 1)
+      ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+    else if (player == 2)
+      ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    if (player == 1)
+      ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
+    else if (player == 2)
+      ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+    return () => {
+      socket.off("disconnect");
+    }
   }, [])
 
   useEffect(() => {
-    const updateOtherplayer = setInterval(() => {
-      const canvas = document.getElementById("canvas");
-      const ctx = canvas?.getContext('2d');
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas?.getContext('2d');
+    if (player == 1)
+      ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
+    else if (player == 2)
       ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
-      setOtherPlayer(value => {
-        if (value < canvas.height-(canvas.height * 0.1))
-          return value + 1
-        else
-          return 0
-      })
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-      ctx.fillRect(0, otherplayer, canvas.width * 0.02, canvas.height * 0.1)
-    }, 10);
-    return () => {
-      clearInterval(updateOtherplayer)
-    }
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    ctx.fillRect(0, otherplayer, canvas.width * 0.02, canvas.height * 0.1)
   }, [otherplayer])
- return (
+
+  socket.on('connect', () => {
+    console.log('connected');
+  })
+
+  socket.on('playerid', (data) => {
+    if (socket.id == data.id){
+      setPlayer(data.player);
+    }
+  })
+
+
+  socket.on('test', (data) => {
+    console.log(data);
+    if (player==1 && data.player == 2)
+      setOtherPlayer(data.position);
+    else if (player==2 && data.player == 1)
+      setOtherPlayer(data.position);
+  });
+
+  return (
   <>
     <canvas id="canvas" width={window.innerWidth/2} height={window.innerHeight/2} className="border-slate-700 border-8" onMouseMove={(evt)=> updateDisplay(evt, document.getElementById("canvas"))} onMouseEnter={(evt)=> updateDisplay(evt, document.getElementById("canvas"))} onMouseLeave={(evt)=> updateDisplay(evt, document.getElementById("canvas"))}></canvas>
   </>
