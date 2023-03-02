63f50a18ec8221586350e1fef79cbdd56ca72961
Alexandre Chane-Law <narguss@MacBook-Air-de-Alexandre.local>
2023-03-02 01:41:21 +0100

    Fix ui

--- test_project/src/App.tsx
+++ test_project/src/App.tsx
@@ -3,39 +3,51 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import io from 'socket.io-client'
 
-const socket = io('http://10.18.199.128:4242', {transports: ['websocket']});
+const socket = io('http://127.0.0.1:4242', {transports: ['websocket']});
 
 function Playground(){
-  const [otherplayer, setOtherPlayer] = useState(0);
-  const [player, setPlayer] = useState(0);
+  const [player_role, setPlayer_role] = useState(0);
+  const [player1, setPlayer1] = useState(0);
+  const [player2, setPlayer2] = useState(0);
 
   function updateDisplay(event, canvas) {
     const ctx = canvas?.getContext('2d');
-    if (player == 1)
+    if (player_role == 1)
       ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
-    else if (player == 2)
+    else if (player_role == 2)
       ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
-    ctx.fillStyle = 'rgba(0, 0, 255, 1)'
     if (event.nativeEvent.offsetY <= 0){
-      if (player == 1)
+      if (player_role == 1){
+        ctx.fillStyle = 'rgba(0, 0, 255, 1)'
         ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
-      else if (player == 2)
-      ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
-      socket.emit('test', {player:player, position: 0});
+      }
+      else if (player_role == 2){
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+        ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
+      }
+      socket.emit('test', {player:player_role, position: 0});
     }
     else if (event.nativeEvent.offsetY < canvas.height-(canvas.height * 0.1)){
-      if (player == 1)
+      if (player_role == 1){
+        ctx.fillStyle = 'rgba(0, 0, 255, 1)'
         ctx.fillRect(0, event.nativeEvent.offsetY, canvas.width * 0.02, canvas.height * 0.1)
-      else if (player == 2)
+      }
+      else if (player_role == 2){
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)'
         ctx.fillRect(canvas.width-(canvas.width * 0.02), event.nativeEvent.offsetY, canvas.width * 0.02, canvas.height * 0.1)
-      socket.emit('test', {player:player, position: event.nativeEvent.offsetY});
+      }
+      socket.emit('test', {player:player_role, position: event.nativeEvent.offsetY});
     }
     else{
-      if (player == 1)
+      if (player_role == 1){
+        ctx.fillStyle = 'rgba(0, 0, 255, 1)'
         ctx.fillRect(0, canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
-      else if (player == 2)
+      }
+      else if (player_role == 2){
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)'
         ctx.fillRect(canvas.width-(canvas.width * 0.02), canvas.height-(canvas.height * 0.1), canvas.width * 0.02, canvas.height * 0.1)
-      socket.emit('test', {player:player, position: canvas.height-(canvas.height * 0.1)});
+      }
+      socket.emit('test', {player:player_role, position: canvas.height-(canvas.height * 0.1)});
     }
   }
 
@@ -44,30 +56,13 @@ function Playground(){
     const canvas = document.getElementById("canvas");
     const ctx = canvas?.getContext('2d');
     ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-    if (player == 1)
-      ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
-    else if (player == 2)
-      ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
+    ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
     ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-    if (player == 1)
-      ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
-    else if (player == 2)
-      ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
+    ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
     return () => {
       socket.off("disconnect");
     }
   }, [])
-
-  useEffect(() => {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas?.getContext('2d');
-    if (player == 1)
-      ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
-    else if (player == 2)
-      ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-    ctx.fillRect(0, otherplayer, canvas.width * 0.02, canvas.height * 0.1)
-  }, [otherplayer])
 
   socket.on('connect', () => {
     console.log('connected');
@@ -75,17 +70,34 @@ function Playground(){
 
   socket.on('playerid', (data) => {
     if (socket.id == data.id){
-      setPlayer(data.player);
+      setPlayer_role(data.player);
     }
   })
+  
 
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas?.getContext('2d');
+
+    ctx.fillStyle = 'rgba(0, 0, 255, 1)'
+    ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
+    ctx.fillRect(0, player1, canvas.width * 0.02, canvas.height * 0.1)
+  }, [player1])
+
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas?.getContext('2d');
+  
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
+    ctx.fillRect(canvas.width-(canvas.width * 0.02), player2, canvas.width * 0.02, canvas.height * 0.1)
+  }, [player2])
 
   socket.on('test', (data) => {
-    console.log(data);
-    if (player==1 && data.player == 2)
-      setOtherPlayer(data.position);
-    else if (player==2 && data.player == 1)
-      setOtherPlayer(data.position);
+    if (data.player == 1)
+      setPlayer1(data.position)
+    else if (data.player == 2)
+      setPlayer2(data.position)
   });
 
   return (
