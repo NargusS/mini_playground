f028f7a0e400542e6bbc1d3744e7374f37cf490a
Narguss <alexandre.chanelaw@icloud.com>
2023-03-08 18:23:39 +0100

    upgrade fix

--- front_ws/src/pages/[id_game].tsx
+++ front_ws/src/pages/[id_game].tsx
@@ -232,6 +232,9 @@ function GamePage({}) {
 	useEffect(() => {
 		if (!socket.connected)
 			socket.connect();
+	},[]);
+
+	useEffect(() => {
 		console.log("Role: " + role + " id_game: " + id_game + " socket: " + socket.connected);
 		if (router.isReady){
 			setId_game(router.query.id_game as string);
@@ -248,11 +251,11 @@ function GamePage({}) {
 				})
 			}
 		}
-	},[socket,router.isReady, id_game, role]);
+	},[router.isReady, id_game, role]);
 
 	return (
 		<div className="w-screen h-screen top-0 left-0 absolute grid grid-cols-1 items-center justify-items-center">
-			{id_game !== "" && role !== 0 ? <Playground role={role} id_game={id_game} socket={socket}/> : <div>Loading...</div> }
+			{id_game !== "" && role !== 0 && socket.connected ? <Playground role={role} id_game={id_game} socket={socket}/> : <div>Loading...</div> }
 		</div>
 	)
 }
