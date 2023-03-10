4a89fab84ec03a1e35c139572b65aa7fb6927459
Narguss <alexandre.chanelaw@icloud.com>
2023-03-10 19:02:40 +0100

    UI Refactor

--- front_ws/src/pages/[id_game].tsx
+++ front_ws/src/pages/[id_game].tsx
@@ -15,61 +15,51 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 	const role = props.role;
 	const id_game = props.id_game;
 	const socket = props.socket;
-	const [size, setSize] = useState({orientation:0, width: 0, height: 0});
+	const [size, setSize] = useState({
+		orientation: -1,
+		width: 0,
+		height: 0
+	});
 	const [player1, setPlayer1] = useState(0);
 	const [player2, setPlayer2] = useState(0);
 	const [prevPos, setPrevPos] = useState(0);
 	
-
-	useEffect(() => {
-		function updateWindowDimensions() {
-			const width = window.innerWidth * 0.8;
-			const height = window.innerHeight * 0.8;
 	
-			if (width > height){
-				if (width > height * (1.75))
-					setSize({orientation:0, width: height*(1.75), height: height});
-				else
-					setSize({orientation:0, width: width, height: width/1.75});
-			}
+	
+	function getCanvasSize(){
+		const width = window.innerWidth * 0.9;
+		const height = window.innerHeight * 0.6;
+	
+		if (width > height){
+			if (width > height * (1.75))
+				setSize({orientation:0, width: height*(1.75), height: height});
 			else
-			{
-				if (height > width * (1.75))
-					setSize({orientation:1,width: width, height: width*(1.75)});
-				else
-					setSize({orientation:1,width: height/1.75, height: height});
-			}
+				setSize({orientation:0, width: width, height: width/1.75});
 		}
-		updateWindowDimensions();
-		window.addEventListener("resize", updateWindowDimensions);
-		return () => window.removeEventListener("resize", updateWindowDimensions);
-	}, [])
+		else
+		{
+			if (height > width * (1.75))
+				setSize({orientation:1,width: width, height: width*(1.75)});
+			else
+				setSize({orientation:1,width: height/1.75, height: height});
+		}
+	}
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		if (!canvas)
-			return;//console.log("canvas is null");
+		return;//console.log("canvas is null");
 		socket.on('UpdateCanvas', (data) => {
 			if (data.player_role == 1)
-		  		setPlayer1(data.position)
+			setPlayer1(data.position)
 			else if (data.player_role == 2)
-		  		setPlayer2(data.position)
+			setPlayer2(data.position)
 		});
-		// const ctx = canvas?.getContext('2d');
-		// if (!ctx)
-		// 	return;//console.log("ctx is null");
-		// if (size.orientation === 0){
-		// 	ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-		// 	ctx.fillRect(0, 0, canvas.width * 0.02, canvas.height * 0.1)
-		// 	ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-		// 	ctx.fillRect(canvas.width-(canvas.width * 0.02), 0, canvas.width * 0.02, canvas.height * 0.1)
-		// }
-		// else{
-		// 	ctx.fillStyle = 'rgba(0, 0, 255, 1)'
-		// 	ctx.fillRect(0, 0, canvas.width * 0.1, canvas.height * 0.02)
-		// 	ctx.fillStyle = 'rgba(255, 0, 0, 1)'
-		// 	ctx.fillRect(0, canvas.height-(canvas.height * 0.02), canvas.width * 0.1, canvas.height * 0.02)
-		// }
+		getCanvasSize();
+		window.addEventListener('resize', getCanvasSize);
+		return () => {
+			window.removeEventListener('resize', getCanvasSize);
+		}
 	}, [])
 
 	function updateDisplay(event:any) {
@@ -90,11 +80,12 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 			if (size.orientation == 0){
 				setPrevPos(event.nativeEvent.offsetY);
 			}
-			else{
+			else if (size.orientation == 1){
 				setPrevPos(event.nativeEvent.offsetX);
 			}
+			else
+				return;
 		}
-
 		if (size.orientation == 0){
 			if (role == 1)
 				ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
@@ -134,8 +125,8 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 				socket.emit('MakeMove', {id_game:id_game,player:role, position: 0.9});
 			}
 		}
-		else{
-			// ORIENTATION 1
+		else if (size.orientation == 1){
+			// size.orientation 1
 			if (role == 1)
 				ctx.clearRect(0, canvas.height - (canvas.height * 0.03), canvas.width, canvas.height* 0.03);
 			else if (role == 2)
@@ -191,7 +182,7 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 			ctx.clearRect(0, 0, canvas.width * 0.03, canvas.height);
 			ctx.fillRect(0, canvas.height * player1, canvas.width * 0.02, canvas.height * 0.1)
 		}
-		else{
+		else if (size.orientation == 1){
 			ctx.clearRect(0, canvas.height - (canvas.height * 0.03), canvas.width, canvas.height * 0.03);
 			ctx.fillRect(player1 * canvas.width, canvas.height - (canvas.height * 0.02), canvas.width * 0.1, canvas.height * 0.02)
 		}
@@ -209,7 +200,7 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 			ctx.clearRect(canvas.width - (canvas.width * 0.03), 0, canvas.width * 0.03, canvas.height);
 			ctx.fillRect(canvas.width-(canvas.width * 0.02), canvas.height*player2, canvas.width * 0.02, canvas.height * 0.1)
 		}
-		else
+		else if (size.orientation == 1)
 		{
 			ctx.clearRect(0, 0, canvas.width, canvas.height * 0.03);
 			ctx.fillRect(player2 * canvas.width, 0, canvas.width * 0.1, canvas.height * 0.02)
@@ -217,11 +208,12 @@ function Playground(props:{role:number, id_game:string, socket:Socket}){
 	}, [player2])
 
 	return (
-		<>
+		<div className="h-[70vh] w-full flex flex-col justify-center items-center overflow-hidden">
 			<canvas id="canvas" ref={canvasRef} width={size.width} height={size.height} className="border-slate-700 border-8" onMouseMove={(evt)=> updateDisplay(evt)}  onMouseLeave={(evt)=> updateDisplay(evt)}></canvas>
-		</>
+		</div>
 	);
 }
+
 
 function GamePage({}) {
 	const router = useRouter();
@@ -235,7 +227,6 @@ function GamePage({}) {
 	},[]);
 
 	useEffect(() => {
-		console.log("Role: " + role + " id_game: " + id_game + " socket: " + socket.connected);
 		if (router.isReady){
 			setId_game(router.query.id_game as string);
 			if (sessionStorage.playerId == undefined){
@@ -254,8 +245,49 @@ function GamePage({}) {
 	},[router.isReady, id_game, role]);
 
 	return (
-		<div className="w-screen h-screen top-0 left-0 absolute grid grid-cols-1 items-center justify-items-center">
+		<div className="w-screen h-screen top-0 left-0 absolute flex flex-col items-center">
+			<div className="bg-blue-100 w-full h-[10vh] inline-flex justify-center items-center">
+				<h1 className="bg-slate-500 w-fit h-min text-[6vh]">1:0</h1>
+			</div>
 			{id_game !== "" && role !== 0 && socket.connected ? <Playground role={role} id_game={id_game} socket={socket}/> : <div>Loading...</div> }
+			<div className="h-[20vh] w-full inline-flex">
+				<div className="grow bg-slate-600 flex flex-col">
+					<h1 className="text-xl w-full h-fit bg-slate-800 text-white text-center">CHAT</h1>
+					<ul className="grow overflow-y-scroll flex flex-col p-3">
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">esafar</p>
+							<p className="text-white font-light">Bonne Game les gars</p>
+						</li>
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">achane-l</p>
+							<p className="text-white font-light">Merci</p>
+						</li>
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">mderome</p>
+							<p className="text-white font-light">Je vais le battre tellement facilement</p>
+						</li>
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">cmenasse</p>
+							<p className="text-white font-light">100€ sur mderome</p>
+						</li>
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">esafar</p>
+							<p className="text-white font-light">T fou mec 150€ sur achane-l</p>
+						</li>
+						<li className="w-fit h-fit">
+							<p className="text-white font-bold">chduong</p>
+							<p className="text-white font-light">On va bien voir</p>
+						</li>
+					</ul>
+					<div className="w-full inline-flex">
+						<input type="text" className="border border-black grow h-full"></input>
+						<button className="border border-black p-2 text-white font-light">SEND</button>
+					</div>
+				</div>
+				<div className="bg-red-200 p-7">
+					<button className="text-2xl border border-black">LEAVE GAME</button>
+				</div>
+			</div>
 		</div>
 	)
 }
